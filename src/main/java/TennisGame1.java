6aeaac3469d820693c41da2ae2ae3f9585e79cc4
CAVALIER <charlotte.cavalier@capgemini.com>
2016-03-28 15:54:24 +0200

    correction exercice

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,10 +1,10 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int scoreP1 = 0;
+    private int scoreP2 = 0;
+    private String player1Name = "player1";
+    private String player2Name = "player2";
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,65 +12,61 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName == player1Name)
+            scoreP1 += 1;
         else
-            m_score2 += 1;
+            scoreP2 += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+        if (scoreP1==scoreP2)
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+            score = findNameScore(scoreP1, true);
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (scoreP1>=4 || scoreP2>=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            score = findWinnerOrAdvantage();
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        	score=findNameScore(scoreP1, false);
+            score+="-";
+            score+=findNameScore(scoreP2, false);
         }
         return score;
     }
+
+	private String findWinnerOrAdvantage() {
+		String score = scoreP1 > scoreP2 ? player1Name : player2Name;
+        return ((scoreP1-scoreP2)*(scoreP1-scoreP2) == 1) ? "Advantage " + score : "Win for " + score;
+	}
+
+    /**
+     * retourne le score en terme tennis
+     * @param scoreNum
+     * @param equality
+     * @return String le score
+     */
+	private String findNameScore(int scoreNum, boolean equality) {
+		String nameScore = "";
+		String equalScore = "-All";
+		switch(scoreNum)
+		{
+		    case 0:
+		    case 1:
+		    case 2:
+		        nameScore=ScoreTitle.getNameFromScore(scoreNum)+( equality?equalScore:"");
+		        break;
+		    case 3:
+		    	if(!equality){
+		    		 nameScore=ScoreTitle.getNameFromScore(scoreNum);
+				     break;
+		    	}
+		    default:
+		    	nameScore=ScoreTitle.DEUCE.getTitle();
+	        break;
+		}
+		return nameScore;
+	}
 }
